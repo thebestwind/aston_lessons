2eb0596917a76db8697da7b5e5416bffd76a331e
slavik <Best_wind@gmail.com>
2024-04-11 20:12:39 +0300

    ryh

---
+++ src/main/java/Lesson3/park.java
@@ -0,0 +1,21 @@
+package Lesson3;
+
+class Park {
+    class Attraction {
+        private String name;
+        private String workingHours;
+        private double cost;
+
+        public Attraction(String name, String workingHours, double cost) {
+            this.name = name;
+            this.workingHours = workingHours;
+            this.cost = cost;
+        }
+
+        public void printInfo() {
+            System.out.println("Аттракцион: " + name);
+            System.out.println("Время работы: " + workingHours);
+            System.out.println("Стоимость: " + cost);
+        }
+    }
+}
