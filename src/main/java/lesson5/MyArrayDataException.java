89405814efa0d7bdd6b8bd641d0f16e2439b2a4e
slavik <Best_wind@gmail.com>
2024-05-27 23:13:14 +0300

    new drill

--- src/main/java/lesson5/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package lesson5;
-
-public class MyArrayDataException extends Exception {
-    public MyArrayDataException(String message) {
-        super(message);
-    }
-}
--- src/main/java/lesson5/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package lesson5;
-
-public class MyArraySizeException extends Exception {
-    public MyArraySizeException(String message) {
-        super(message);
-    }
-}
