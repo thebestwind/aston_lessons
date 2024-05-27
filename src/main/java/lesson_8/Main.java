096a8cad71e4296cc86deacf55a41cc7b010a5f3
slavik <Best_wind@gmail.com>
2024-05-27 23:20:03 +0300

    new drill

--- src/main/java/lesson_8/Main.java
+++ src/main/java/lesson_8/Main.java
@@ -1,4 +1,16 @@
 package lesson_8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.Duration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import jdk.jfr.Description;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
 }
