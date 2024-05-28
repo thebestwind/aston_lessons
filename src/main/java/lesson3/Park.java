df3833205b9c2be5a67cd6af2a58918659fb5839
slavik <Best_wind@gmail.com>
2024-05-28 21:29:25 +0300

    new drill

---
+++ src/main/java/lesson3/Park.java
@@ -0,0 +1,21 @@
+package lesson3;
+
+class Park {
+    static class Attraction {
+        private String name;
+        private String workingHours;
+        private double cost;
+
+        public Attraction(String name, String workingHours, double cost) {
+            this.name = name;
+            this.workingHours = workingHours;
+            this.cost = cost;
+        }
+
+        public void printInfo() {
+            System.out.println("Аттракцион: " + name);
+            System.out.println("Время работы: " + workingHours);
+            System.out.println("Стоимость: " + cost);
+        }
+    }
+}
