bb86f999bde7a72ffbf2f2d75d1cd72fdb5b58d6
slavik <Best_wind@gmail.com>
2024-05-29 10:22:45 +0300

    new drill

--- src/main/java/lesson7_junit_5/FactorialTest.java
+++ src/main/java/lesson7_junit_5/FactorialTest.java
@@ -1,18 +1,21 @@
 package lesson7_junit_5;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FactorialTest {
     @Test
-    public void testFactorial() {
+    void testFactorial() {
         assertEquals(1, Main.factorial(0));
         assertEquals(1, Main.factorial(1));
         assertEquals(2, Main.factorial(2));
         assertEquals(6, Main.factorial(3));
         assertEquals(24, Main.factorial(4));
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Main.factorial(-3),
-                "Expected getFactorial() to throw, but it didn't");
-        assertEquals("Введите неотрицательное число", exception.getMessage());
+    }
+
+    @Test
+    void testFactorialNegativeNumber() {
+        assertThrows(IllegalArgumentException.class, () -> Main.factorial(-1), "Expected factorial to throw, but it didn't");
     }
 }
--- src/main/java/lesson7_junit_5/Main.java
+++ src/main/java/lesson7_junit_5/Main.java
@@ -12,15 +12,13 @@ public class Main {
     }
 
     public static int factorial(int num) {
-        int factorial = 1;
         if (num < 0) {
             throw new IllegalArgumentException("Введите неотрицательное число");
-        } else {
-        for(int i = 1; i <= num; ++i) {
+        }
+        int factorial = 1;
+        for (int i = 1; i <= num; ++i) {
             factorial *= i;
         }
         return factorial;
     }
-}}
-
-
+}
